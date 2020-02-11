ea60507e0553f8ce4d78934b4b57b263a496e573
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-11 14:01:50 -0500

    Add claases to game and player.

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-const canvas = document.querySelector("#canvas");
-let context = canvas.getContext("2d");
+class Game {
+  constructor() {
+    let canvas = document.querySelector("#canvas");
+    let screen = canvas.getContext("2d");
+    this.player = new Player(screen);
+  }
+}
+
+class Player {
+  constructor(screen) {
+    this.player = this.draw(screen);
+  }
+  draw(screen) {
+    screen.beginPath();
+    screen.rect(20, 20, 30, 30);
+    screen.fillStyle = "#fff";
+    screen.fill();
+    screen.closePath();
+  }
+}
+
+let game = new Game();
