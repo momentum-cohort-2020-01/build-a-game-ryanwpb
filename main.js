c8fa7ff23b6b70598b511a42a005f9dd45a64710
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-12 14:55:30 -0500

    Center player on bottom of game screen.

--- main.js
+++ main.js
@@ -2,20 +2,23 @@ class Game {
   constructor() {
     let canvas = document.querySelector("#canvas");
     let screen = canvas.getContext("2d");
-    this.player = new Player(screen);
+    this.player = new Player(screen, this);
   }
 }
 
 class Player {
   constructor(screen) {
     this.player = this.draw(screen);
+    this.screenWidth = screen.width;
   }
   draw(screen) {
-    screen.beginPath();
-    screen.rect(20, 20, 30, 30);
     screen.fillStyle = "#ff5a5f";
-    screen.fill();
-    screen.closePath();
+    screen.fillRect(
+      canvas.width / 2 - 3,
+      canvas.height - canvas.height / 5,
+      30,
+      30
+    );
   }
 }
 
