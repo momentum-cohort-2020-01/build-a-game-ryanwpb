648d385144197a8e5d7856e6e12dc9dd0b54ff33
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-13 17:59:37 -0500

    Respawn enemies if they leave the canvas.

--- main.js
+++ main.js
@@ -20,6 +20,20 @@ class Game {
     for (let i = 0; i < this.bodies.length; i++) {
       this.bodies[i].update();
     }
+
+    for (let i = 0; i < this.bodies.length; i++) {
+      if (
+        this.bodies[i].center.y > 800 ||
+        this.bodies[i].center.x > canvas.width ||
+        this.bodies[i].center.y < 0 ||
+        this.bodies[i].center.x < 0
+      ) {
+        this.bodies.splice(i, 1);
+        this.bodies.push(
+          new Enemy(this, { x: Math.random() * 500, y: Math.random() * 700 })
+        );
+      }
+    }
   }
 
   draw(screen, gameSize) {
@@ -39,12 +53,14 @@ class Player {
     this.game = game;
     this.size = { x: 20, y: 20 };
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 };
+
     this.keyboarder = Keyboarder;
   }
 
   update() {
     if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
       this.center.x -= 2;
+      console.log(this.center);
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
       this.center.x += 2;
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
@@ -61,13 +77,21 @@ class Enemy {
     this.center = center;
     this.size = { x: 20, y: 20 };
     this.moveX = 0;
-    this.speedX = 0.5;
+    this.speedX = Math.random() * 5 - 2.5;
+    this.moveY = 0;
+    this.speedY = Math.random() * 5 - 2.5;
   }
 
   update() {
     this.center.x += this.speedX;
+    this.center.y += this.speedY;
     this.moveX += this.speedX;
+    this.moveY += this.speedY;
     if (this.moveX < 0 || this.moveX > 30) {
+      // ... reverse direction of movement.
+      this.speedX = -this.speedX;
+    }
+    if (this.moveY < 0 || this.moveY > 30) {
       // ... reverse direction of movement.
       this.speedX = -this.speedX;
     }
